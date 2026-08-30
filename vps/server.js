8818d82c3b84a0a0e9bba6d339b44c11ee36d190
davletovfaxriddin5-gif <davletovfaxriddin5@gmail.com>
2026-08-30 23:32:37 +0300

    Add files via upload

--- vps/server.js
+++ vps/server.js
@@ -492,7 +492,7 @@ function loadGames(){
     });
     if(cats.length) APPGAMES.push({
       id:g.id, name:g.name, glyph: GLYPH[g.id] || "\uD83C\uDFAE", img: g.img || "",
-      maint: !!g.maint, cats: cats
+      vid: g.vid || "", maint: !!g.maint, cats: cats
     });
   });
   console.log("games.json: " + APPGAMES.length + " o'yin, " + Object.keys(GIDX).length + " paket");
